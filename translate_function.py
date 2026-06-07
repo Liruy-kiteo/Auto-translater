6ff6983a007daf77284d80925013731bbcabb2ff
liruykiteo@gmail.com <liruykiteo@gmail.com>
2026-06-07 17:29:30 +0300

    добавил OCR сразу в файл, чтобы не нужно было скачивать зависимости(я не подумаал об этом сразу)

--- translate_function.py
+++ translate_function.py
@@ -13,10 +13,12 @@ class Translate():
       #берет последнее изображение из clipboard
       self.img = ImageGrab.grabclipboard()
 
+      pytesseract.pytesseract.tesseract_cmd = r'D:\\programming\\Projects\\Auto translater\\tesseract\\tesseract.exe'
+
       #переводит изображение в формат который читает tesseract, нужен для того чтобы pytesseract мог работать с alt+prtsc
       #из-за того что он сохраняет данные в bitmap(если не ошибаюсь), а не png
       self.text_from_img = self.img.convert("RGB")
-      
+
       #подсмотрел это решение, чтобы изображение лучше переводилось в текст
       self.img = self.img.convert('L')
 
