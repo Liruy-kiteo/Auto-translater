43186d83c8b482e3804ce78baed2e81802d7d167
liruykiteo@gmail.com <liruykiteo@gmail.com>
2026-06-09 02:06:36 +0300

    убран путь к teseract

--- translate_function.py
+++ translate_function.py
@@ -13,8 +13,6 @@ class Translate():
       #берет последнее изображение из clipboard
       self.img = ImageGrab.grabclipboard()
 
-      pytesseract.pytesseract.tesseract_cmd = r'D:\\programming\\Projects\\Auto translater\\tesseract\\tesseract.exe'
-
       #переводит изображение в формат который читает tesseract, нужен для того чтобы pytesseract мог работать с alt+prtsc
       #из-за того что он сохраняет данные в bitmap(если не ошибаюсь), а не png
       self.text_from_img = self.img.convert("RGB")
